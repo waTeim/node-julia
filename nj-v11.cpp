8be44221208093b29cb52973deb021b68b73e879
waTeim <truthset@gmail.com>
2014-08-10 00:34:04 -0400

    Array fill

--- nj-v11.cpp
+++ nj-v11.cpp
@@ -195,20 +195,64 @@ nj::Type *getPrimitiveType(const Local<Value> &prim)
    return 0;
 }
 
-shared_ptr<nj::Value> buildArrayReq(const Local<Value> &value)
+template <typename V,typename E> void fillArrayReq(shared_ptr<nj::Value> &to,const Local<Array> &from)
 {
-   shared_ptr<nj::Value> v;
+    nj::Array<V,E> &a = static_cast<nj::Array<V,E>&>(*to);
+    const nj::Array_t *atype = static_cast<const nj::Array_t*>(a.type());
+    V *p = a.ptr();
 
-   if(value->IsArray())
+   if(a.dims().size() == 1)
    {
-      Local<Array> a = Local<Array>::Cast(value);
+      size_t length = a.dims()[0];
+
+      for(size_t index = 0;index < length;index++)
+      {
+         switch(atype->etype()->getId())
+         {
+            case nj::boolean_type: *p++ = from->Get(index)->BooleanValue(); break;
+            case nj::int32_type: *p++ = from->Get(index)->Int32Value(); break;
+            case nj::uint32_type: *p++ = from->Get(index)->Uint32Value(); break;
+            case nj::float64_type: *p++ = from->Get(index)->NumberValue(); break;
+         }
+      }
+   }
+   else if(a.dims().size() == 2)
+   {
+      size_t rows = a.dims()[0];
+      size_t cols = a.dims()[1];
+
+      for(size_t row = 0;row < rows;row++)
+      {
+         Local<Array> rowVector = Local<Array>::Cast(from->Get(row));
+
+         for(size_t col = 0;col < cols;col++)
+         {
+            switch(atype->etype()->getId())
+            {
+               case nj::boolean_type: p[col*rows + row] = rowVector->Get(col)->BooleanValue(); break;
+               case nj::int32_type: p[col*rows + row] = rowVector->Get(col)->Int32Value(); break;
+               case nj::uint32_type: p[col*rows + row] = rowVector->Get(col)->Uint32Value(); break;
+               case nj::float64_type: p[col*rows + row] = rowVector->Get(col)->NumberValue(); break;
+            }
+         }
+      }
+   }
+}
+
+shared_ptr<nj::Value> buildArrayReq(const Local<Value> &from)
+{
+   shared_ptr<nj::Value> to;
+
+   if(from->IsArray())
+   {
+      Local<Array> a = Local<Array>::Cast(from);
       vector<size_t> dims;
 
       dims.push_back(a->Length());
       if(dims[0] == 0)
       {
-         v.reset(new nj::Array<char,nj::Any_t>(dims));
-         return v;
+         to.reset(new nj::Array<char,nj::Any_t>(dims));
+         return to;
       }
 
       Local<Value> el = a->Get(0);
@@ -217,7 +261,7 @@ shared_ptr<nj::Value> buildArrayReq(const Local<Value> &value)
       {
          a = Local<Array>::Cast(el);
          dims.push_back(a->Length());
-         if(dims[0] == 0) return v;
+         if(dims[0] == 0) return to;
          el = a->Get(0);
       }
       if(!el->IsObject())
@@ -228,15 +272,27 @@ shared_ptr<nj::Value> buildArrayReq(const Local<Value> &value)
          {
             switch(etype->getId())
             {
-               case nj::boolean_type: v.reset(new nj::Array<bool,nj::Boolean_t>(dims)); break;
-               case nj::int32_type: v.reset(new nj::Array<int,nj::Int32_t>(dims)); break;
-               case nj::uint32_type: v.reset(new nj::Array<unsigned int,nj::UInt32_t>(dims)); break;
-               case nj::float64_type: v.reset(new nj::Array<double,nj::Float64_t>(dims)); break;
+               case nj::boolean_type:
+                  to.reset(new nj::Array<bool,nj::Boolean_t>(dims));
+                  fillArrayReq<bool,nj::Boolean_t>(to,a);
+               break;
+               case nj::int32_type:
+                  to.reset(new nj::Array<int,nj::Int32_t>(dims));
+                  fillArrayReq<int,nj::Int32_t>(to,a);
+               break;
+               case nj::uint32_type:
+                  to.reset(new nj::Array<unsigned int,nj::UInt32_t>(dims));
+                  fillArrayReq<unsigned int,nj::UInt32_t>(to,a);
+               break;
+               case nj::float64_type:
+                  to.reset(new nj::Array<double,nj::Float64_t>(dims));
+                  fillArrayReq<double,nj::Float64_t>(to,a);
+               break;
             }
          }
       }
    }
-   return v;
+   return to;
 }
 
 shared_ptr<nj::Value> buildReq(const Local<Value> &value)
