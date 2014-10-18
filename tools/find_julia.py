fd87c157449a3231abe335d8a7e3c6bf58b96e74
waTeim <truthset@gmail.com>
2014-10-18 17:06:31 -0400

    Added support for detecting Julia in windows.

--- tools/find_julia.py
+++ tools/find_julia.py
@@ -13,10 +13,31 @@ def julia_from_which_julia():
          path,tail = os.path.split(head)
    return path 
 
+def julia_from_where_julia():
+   path = "";
+   DEVNULL = open(os.devnull, 'w')
+   where_julia = subprocess.Popen(["where","julia.exe"],stdout=subprocess.PIPE,stderr=DEVNULL).communicate()[0];
+   if len(where_julia) > 0:
+      real_path = os.path.realpath(where_julia.split()[0])
+      if real_path:
+         head,tail = os.path.split(real_path)
+         path,tail = os.path.split(head)
+   return path 
+
 def julia_from_home_directory():
    home = os.path.expanduser("~")
    julia_dir = os.path.join(home,"julia")
    if os.path.isdir(julia_dir): return julia_dir
+   return "";
+
+def julia_from_home_directory_win():
+   home = os.path.expanduser("~")
+   search_folder = os.path.join(home, "AppData\Local")
+   DEVNULL = open(os.devnull, 'w')
+   if os.path.isdir(search_folder):
+      where_julia = subprocess.Popen(["where","julia.exe", "/r", search_folder],stdout=subprocess.PIPE,stderr=DEVNULL).communicate()[0];
+      julia_dir,filename = os.path.split(where_julia)
+      if os.path.isdir(julia_dir): return julia_dir
    return "";
 
 def julia_from_applications():
@@ -24,8 +45,12 @@ def julia_from_applications():
    if os.path.isdir(julia_dir): return julia_dir
    return ""
 
-if sys.argv[1] == "mac" or sys.argv[1] == "linux": path = julia_from_which_julia()
-if path == "": path = julia_from_home_directory()
-if path == "" and len(sys.argv) > 1 and sys.argv[1] == "mac": path = julia_from_applications()
+if sys.argv[1] == "win":
+   path = julia_from_where_julia()
+   if path == "": path = julia_from_home_directory_win()
+else:
+   path = julia_from_which_julia()
+   if path == "": path = julia_from_home_directory()
+   if path == "" and len(sys.argv) > 1 and sys.argv[1] == "mac": path = julia_from_applications()
 
 if not path == "": print path
