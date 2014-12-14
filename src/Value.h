beec94b9dac84bb7170a19586beaed6a8049e545
waTeim <truthset@gmail.com>
2014-12-14 05:02:20 -0500

    Further throw decl changes for gcc

--- src/Value.h
+++ src/Value.h
@@ -63,6 +63,7 @@ namespace nj
          virtual const Type *type() const {  return Array_t::instance(E::instance());  }
          virtual V *ptr() const {  return data.get()->data();  }
          virtual size_t size() const {  return num_elements;  }
+         virtual ~Array() throw(JuliaException) {}
    };
 };
 
