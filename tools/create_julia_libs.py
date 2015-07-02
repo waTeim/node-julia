ae37dea92e61b48542723c632c49f5553f851232
unknown <mercutio216@gmail.com>
2015-07-01 22:13:40 -0400

    previous commit missed the file changes

--- tools/create_julia_libs.py
+++ tools/create_julia_libs.py
@@ -1,12 +1,12 @@
-import re
+import argparse
 import os.path 
 import StringIO
 import subprocess
 import sys 
 import _winreg 
 
-# This script will create libjulia.lib from libjulia.dll using the C tools
-# provided by Microsoft Visual Studio.
+# This script will create a .lib from a given .dll using the C tools provided
+# by Microsoft Visual Studio.
 
 # Please note on 64-bit versions of Windows, registry operations to "HKLM\Software\..."
 # are redirected to "HKLM\Software\Wow6432Node\..." 
@@ -17,27 +17,43 @@ VISUAL_STUDIO_2012_REGISTRY_PATH  = r"Software\Microsoft\VisualStudio\11.0"
 #VISUAL_STUDIO_2012_REGISTRY_PATH  = r"Software\Wow6432Node\Microsoft\VisualStudio\11.0" 
 VISUAL_STUDIO_SHELL_FOLDER_KEY    = "ShellFolder" 
 VISUAL_C_BIN_FOLDER               = r"VC\bin"
-LIBJULIA_DLL                      = "libjulia.dll"
-LIBJULIA_DEF                      = "libjulia.def"
-LIBJULIA_LIB                      = "libjulia.lib"
 DUMPBIN_EXPORT_HEADER             = "ordinal hint"
 DUMPBIN_EXPORT_FOOTER             = "Summary"
 
-def usage():
-   print "Usage: create_libjulia_lib <path_to_libjulia_bin_folder>"
+# Override argparse's default error handler so that it displays the "full" help, rather
+# than an abbreviated one on errors.
+class MyParser(argparse.ArgumentParser): 
+   def error(self, message):
+      sys.stderr.write('Error: %s\n' % message)
+      self.print_help()
+      sys.exit(1)
 
-def get_libjulia_path():
-   if len(sys.argv) != 2:
-      usage()
+def parse_command_line():
+   try:
+      parser = MyParser()
+	  
+	  # Add the required arguments to a separate group.  If we don't do this, print_help will
+	  # show them under "optional arguments", even though they are marked as required.
+      required_args = parser.add_argument_group('required arguments')
+      required_args.add_argument('--bin', dest = 'bin_folder', required=True, help = 'path to the Julia bin folder')
+      required_args.add_argument('--files', dest = 'files', required=True, nargs = '+', help = 'list of dll files to process')
+
+      args = parser.parse_args()
+   except Exception, e: 
+#      print str(e) 
+      parser.print_help()
+      pass 
       sys.exit(1)
-   julia_path = sys.argv[1]
-   if not os.path.exists(julia_path): 
-      print "ERROR: Unable to find " + julia_path 
+
+   if not os.path.exists(args.bin_folder): 
+      print "ERROR: Unable to find " + args.bin_folder 
       sys.exit(1)
-   if not os.path.exists(os.path.join(julia_path, LIBJULIA_DLL)):
-      print "ERROR: Unable to find " + LIBJULIA_DLL + " at " + julia_path 
-      sys.exit(1)
-   return julia_path
+   for dll_name in args.files:
+      dll_file_path = os.path.join(args.bin_folder, dll_name)
+      if not os.path.exists(dll_file_path):
+         print "ERROR: Unable to find " + dll_file_path
+         sys.exit(1)
+   return args.bin_folder, args.files
 
 # Get a registry value from HKEY_LOCAL_MACHINE 
 def get_hklm_value(reg_path, reg_key): 
@@ -48,7 +64,7 @@ def get_hklm_value(reg_path, reg_key):
       hKey = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, reg_path, _winreg.KEY_READ) 
       value, type = _winreg.QueryValueEx(hKey, reg_key) 
    except Exception, e: 
-      print str(e) 
+      #print str(e) 
       pass 
    finally: 
       if hKey != None: 
@@ -71,20 +87,21 @@ def get_symbol_name(temp):
 
    return sname
 
-def create_def_file(vc_bin_path, libjulia_bin_path): 
+def create_def_file(dll_file_name, libjulia_bin_path, vc_bin_path): 
    dumpbin_path = os.path.join(vc_bin_path, "dumpbin.exe")
    if not os.path.exists(dumpbin_path): 
       print "ERROR: Unable to find dumpbin at: " + dumpbin_path 
       sys.exit(1)
-   dll_path = os.path.join(libjulia_bin_path, LIBJULIA_DLL)
+   dll_basename = os.path.splitext(dll_file_name)[0]
+   dll_path = os.path.join(libjulia_bin_path, dll_file_name)
 
    # Have dumpbin extract all the exports from the dll
    exports = subprocess.Popen([dumpbin_path, "/exports", dll_path],stdout=subprocess.PIPE).communicate()[0]
    #print exports
 
-   def_file = os.path.join(libjulia_bin_path, LIBJULIA_DEF)
+   def_file = os.path.join(libjulia_bin_path, dll_basename + '.def')
    fp = open(def_file,'w')
-   fp.write('LIBRARY LIBJULIA\n')
+   fp.write('LIBRARY ' + dll_basename.upper() + '\n')
    fp.write('EXPORTS\n')
 
    found_header = False
@@ -102,26 +119,27 @@ def create_def_file(vc_bin_path, libjulia_bin_path):
    fp.close()
    return def_file
 
-def create_lib(vc_bin_path, libjulia_bin_path): 
+def create_lib(dll_file_name, libjulia_bin_path, vc_bin_path): 
    lib_exe_path = os.path.join(vc_bin_path, "lib.exe")
    if not os.path.exists(lib_exe_path): 
       print "ERROR: Unable to find lib at: " + lib_exe_path 
       sys.exit(1)
 
-   def_file_path = os.path.join(libjulia_bin_path, LIBJULIA_DEF)
-   lib_file_path = os.path.join(libjulia_bin_path, LIBJULIA_LIB)
+   dll_basename = os.path.splitext(dll_file_name)[0]
+   def_file_path = os.path.join(libjulia_bin_path, dll_basename + '.def')
+   lib_file_path = os.path.join(libjulia_bin_path, dll_basename + '.lib')
 
-   results = subprocess.Popen([lib_exe_path, "/def:" + def_file_path, "/OUT:" + lib_file_path, "/machine:x64"],stdout=subprocess.PIPE).communicate()[0]
+   results = subprocess.Popen([lib_exe_path, "/def:" + def_file_path, "/out:" + lib_file_path, "/machine:x64"],stdout=subprocess.PIPE).communicate()[0]
    #print results
    return lib_file_path
 
-# Find libjulia
-libjulia_bin_path = get_libjulia_path();
+# Get our Julia bin path and a list of dlls from the command line
+libjulia_bin_path, dll_list = parse_command_line()
 
 # Get the Visual Studio installation folder
 vs_path = find_visual_studio() 
 if vs_path == None: 
-   print "ERROR: Unable to find Visual Studio installation folder." 
+   print "ERROR: Unable to find the Visual Studio installation folder." 
    sys.exit(1) 
 
 # Make sure they installed Visual C++
@@ -130,11 +148,11 @@ if not os.path.exists(vc_bin_path):
    print "ERROR: Unable to find Visual C bin folder at: " + vc_bin_path 
    sys.exit(1) 
 
-# Create our definition file
-def_file_path = create_def_file(vc_bin_path, libjulia_bin_path) 
-print "Created " + def_file_path
+# Create our definition files
+for dll_file_name in dll_list:
+   def_file_path = create_def_file(dll_file_name, libjulia_bin_path, vc_bin_path) 
+   print "Created " + def_file_path
 
-# Create libjulia.lib
-lib_file_path = create_lib(vc_bin_path, libjulia_bin_path)
-print "Created " + lib_file_path
-
+   # Create our .lib file
+   lib_file_path = create_lib(dll_file_name, libjulia_bin_path, vc_bin_path)
+   print "Created " + lib_file_path
